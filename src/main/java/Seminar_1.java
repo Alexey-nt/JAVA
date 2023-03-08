b599208afdb67ca769bbd8e63fc17080b805f66a
Alexey <alexey-nt@mail.ru>
2023-03-08 22:44:23 +0500

    Seminar 1 and 2

--- src/main/java/Seminar_1.java
+++ src/main/java/Seminar_1.java
@@ -1,21 +1,60 @@
+import java.util.Random;
+
 public class Seminar_1 {
 
-    // примитивные типы данных
-    int i; //32bit
-    public  int j;  // переменная будет видна за классом main
-    private  int k;  // переменная НЕ будет видна за классом main
-    long l; //64bit
-    byte b; //8bit
+    static int i = 1; //32bit
+    static long l = 6; //64bit
+    static byte b; //8bit
     short sh; //16bit
     float f; //32bit floating poin
     double d; //64bit floating poin
-    boolean aBoolean;
-    char aChar; //16bit без знака
+    static boolean aBoolean;
+    char aChar = 65; //16бит без знака
+    String string = "Привет Мир!";
+    static String tmpS = new String("Привет Мир!");
 
-    // ссылочный тип данных
-    String string = "Привет мир!";  // так можно обьявить и инициализировать строку
-    String string1; // создание пустой строки
-    String string2 = "";  // тоже создание пустой строки
-    String tmpS = new String("Привет мир!");  // Правильное обьявление и инициализация переменной ссылочного типа
+    static int[] ints = new int[10];
 
+    public static void main(String[] args) {
+
+        ints[3] = 10;
+        int[] tmp = new int[ints.length * 2 + 1];
+
+        Random random = new Random();
+
+        for (int j=0 ; j<ints.length ; j++) {
+            tmp[j] = ints[j] + random.nextInt(100);
+        }
+        tmp[15] = 5;
+        ints = tmp;
+
+        for (int i: ints) {
+            System.out.println(i);
+        }
+
+
+        if (aBoolean) {
+            getInt(String.valueOf(12));
+        } else if ( l > i) {
+            getInt(String.valueOf(13));
+        } else {
+            getInt(String.valueOf(14));
+        }
+
+        System.out.println(Integer.toBinaryString(158));
+        System.out.println(
+                Integer.toBinaryString(158).length()
+        );
+
+    }
+
+    static int getInt(String strToInt){
+        int i = 0;
+        Seminar_1.l = i;
+        return Integer.parseInt(strToInt);
+    }
+
+    static void print(String str){
+        System.out.println(str);
+    }
 }
